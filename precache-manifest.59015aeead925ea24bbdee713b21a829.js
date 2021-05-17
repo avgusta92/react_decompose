a2f675f06ff4e1c18821e18d0dc322fe357d5138
avgusta92 <avgusta92@gmail.com>
2021-05-17 21:03:22 +0300

    Updates

---
+++ precache-manifest.59015aeead925ea24bbdee713b21a829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3dcf6d7c88c5757fec43f74b347982",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1acafb918a2f9c4a9686",
+    "url": "./static/css/main.5e7a641d.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acafb918a2f9c4a9686",
+    "url": "./static/js/main.30887aad.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
